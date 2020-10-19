cb263aae52d097140c315218c593ec2df3ae6bb1
Rafael Fernández <rafael.fernandez@sdos.es>
2020-10-19 12:22:39 +0200

    Add sample for SPM

---
+++ Sample/SDOSL10n/AppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTest
+//
+//  Created by Rafael Fernandez Alvarez on 19/10/20.
+//  Copyright © 2020 Rafael Fernandez Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
